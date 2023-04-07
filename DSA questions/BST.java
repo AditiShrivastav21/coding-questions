3fafe9ed24d2b8f763affc3abe36bd9b3f95b678
Aditi Sanjay Shrivastav <104912256+AditiShrivastav21@users.noreply.github.com>
2023-04-07 15:32:40 +0530

    Add files via upload

---
+++ DSA questions/BST.java
@@ -0,0 +1,65 @@
+//building a binary search tree
+public class BST
+{
+	static class node
+	{	//declaring data members of node
+		int data; 
+		node left; //left child node of the class node
+		node right; // right child node of the class node
+	
+	node(int data)
+	{
+		this.data=data;
+	}
+	
+	}
+	
+	public static node insert(node root, int val)
+	{
+		if(root==null) // if condition to check whether root node is null or not
+		{
+			root = new node(val); // new node value will be now the root value
+			return root;
+		}
+		
+		if(root.data>val)
+		{
+			//left subtree
+			root.left= insert(root.left, val);
+		}
+		
+		else
+		{
+			//right subtree
+			root.right= insert(root.right, val);
+		}
+			
+		return root;
+		
+	}
+	
+	public static void inorder(node root)
+	{
+		if(root==null)
+		{
+			return;
+		}
+		
+		inorder(root.left);
+		System.out.print(root.data+"");
+		inorder(root.right);
+	}
+	public static void main(String args[])
+	{
+	int values[]={5,1,3,4,2,7};
+	node root =null; // declaring root variable of type node
+	for(int i=0; i<values.length; i++) // traversal of values inside the tree
+	{
+		root= insert(root, values[i]);
+	}
+	
+	inorder(root);
+	System.out.println();
+	
+	}
+}
